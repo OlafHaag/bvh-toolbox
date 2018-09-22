e8375eeacdd16e1058e60c873344f054c7efa63a
Olaf Haag <haag.olaf@gmail.com>
2018-09-22 16:56:47 +0200

    moved dependencies into setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,46 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+from os import path
+from io import open
 
-setup(name='bvhtools',
-      version='0.1',
-      description='Python module for manipulating and converting BVH mocap files',
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
+setup(name='bvhtoolbox',
+      version='0.1a2',
+      description='Python module for reading, manipulating and converting BVH mocap files',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Olaf Haag',
       author_email='haag.olaf@gmail.com',
-      url='https://github.com/olafhaag/bvh-tools',
-      py_modules=['converters'],
+      url='https://github.com/olafhaag/bvh-toolbox',
+      classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+      ],
+      keywords='bvh BioVision mocap convert animation 3d',
+      packages=find_packages(exclude=['converters/example_files']),
+      install_requires=['bvh > 0.3',  # You may have to get version >0.3 from https://github.com/20tab/bvh-python.
+                        'numpy',  # >= 1.15
+                        'transforms3d >= 0.3.1'],
+      extras_require={'dev': ['sympy', 'panda3d'],
+                      },
+      entry_points={'console_scripts': ['bvh2csv=converters/bvh2csv_batch:main',
+                                        'bvh2egg=converters/bvh2egg_batch:main',
+                                        'bvh2xaf=converters/bvh2xaf_batch:main',
+                                        'bvh2xsf=converters/bvh2xsf:main',
+                                        ],
+                    },
+      project_urls={
+                    'Bug Reports': 'https://github.com/olafhaag/bvh-toolbox/issues',
+                    'Source': 'https://github.com/olafhaag/bvh-toolbox/',
+                   },
       )
