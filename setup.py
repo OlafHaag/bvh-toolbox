2505222d1ba12416b6ea3fa2a0d46fca0550a975
Olaf Haag <4630661+olafhaag@users.noreply.github.com>
2018-09-23 04:50:59 +0200

    moved modules and packages inside bvhtoolbox folder

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+""" Installation script for bvhtoolbox package """
 
 try:
     from setuptools import setup, find_packages
@@ -21,25 +22,30 @@ setup(name='bvhtoolbox',
       author='Olaf Haag',
       author_email='haag.olaf@gmail.com',
       url='https://github.com/olafhaag/bvh-toolbox',
-      classifiers=[
-        'License :: OSI Approved :: MIT License',
-        #'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-      ],
+      classifiers=['License :: OSI Approved :: MIT License',
+                   #'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Operating System :: OS Independent',
+                   'Environment :: Console',
+                   ],
       keywords='bvh BioVision mocap convert animation 3d',
-      packages=find_packages(exclude=['convert/example_files']),
+      packages=find_packages(exclude=['bvhtoolbox/convert/example_files',
+                                      'bvhtoolbox/convert/bvh2xaf_helpers',
+                                      'bvhtoolbox/convert/ElementTree_pretty',
+                                      'bvhtoolbox/convert/egg_viewer',
+                                      ]),
       install_requires=['bvh > 0.3',  # You may have to get version >0.3 from https://github.com/20tab/bvh-python.
                         'numpy',  # >= 1.15
                         'transforms3d >= 0.3.1'],
       extras_require={'dev': ['sympy', 'panda3d'],
                       },
-      entry_points={'console_scripts': ['bvh2csv=convert/bvh2csv_batch:main',
-                                        'bvh2egg=convert/bvh2egg_batch:main',
-                                        'bvh2xaf=convert/bvh2xaf_batch:main',
-                                        'bvh2xsf=convert/bvh2xsf:main',
-                                        'bvhoffsetjointangles=manipulate/offsetjointangles:main',
-                                        'bvhremoveframes=manipulate/removeframes:main'
-                                        'bvhrenamejoints=manipulate/renamejoints:main'
+      entry_points={'console_scripts': ['bvh2csv=bvhtoolbox/convert/bvh2csv_batch:main',
+                                        'bvh2egg=bvhtoolbox/convert/bvh2egg_batch:main',
+                                        'bvh2xaf=bvhtoolbox/convert/bvh2xaf_batch:main',
+                                        'bvh2xsf=bvhtoolbox/convert/bvh2xsf:main',
+                                        'bvhoffsetjointangles=bvhtoolbox/manipulate/offsetjointangles:main',
+                                        'bvhremoveframes=bvhtoolbox/manipulate/removeframes:main'
+                                        'bvhrenamejoints=bvhtoolbox/manipulate/renamejoints:main'
                                         ],
                     },
       project_urls={'Bug Reports': 'https://github.com/olafhaag/bvh-toolbox/issues',
